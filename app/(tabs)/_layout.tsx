8cf106152a55b46c6ee82bdfe4329cec6529f50b
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-08-22 18:42:00 +1200

    Remove two route
    feat: remove two route

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -51,12 +51,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="two"
-        options={{
-          href: null,
-        }}
-      />
     </Tabs>
   );
 }
