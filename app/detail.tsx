72fcc6c9248b490d77684eb06dee3135157a82aa
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-08-22 18:14:23 +1200

    Rename detail route function
    refactor: Rename detail route function since it won't bother the rendering of the route content

--- app/detail.tsx
+++ app/detail.tsx
@@ -1,12 +1,12 @@
 import { useLocalSearchParams } from 'expo-router';
 import { Text, View } from 'src/components/Themed';
 
-export default function DetailScreen() {
+export default function Route() {
   const { id } = useLocalSearchParams();
   return (
     <View>
       <Text>Detail Screen</Text>
-      <Text>ID: ${id}</Text>
+      <Text>ID: {id}</Text>
     </View>
   );
 }
