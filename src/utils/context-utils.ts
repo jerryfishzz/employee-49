359f3ddaee9cd0dbbbfaa313507be7fefb11b34e
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-08-27 10:36:51 +1200

    Create context-utils.ts
    feat: create custom createContext function in context-utils

---
+++ src/utils/context-utils.ts
@@ -0,0 +1,24 @@
+import { createContext, useContext } from 'react';
+
+function customCreateContext<A extends unknown | null>(
+  providerName: string,
+  displayName?: string,
+) {
+  const ctx = createContext<A | undefined>(undefined);
+
+  if (ctx && displayName) {
+    ctx.displayName = displayName;
+  }
+
+  function useCtx(componentName: string = 'Consumer components') {
+    const c = useContext(ctx);
+    if (c === undefined)
+      throw new Error(
+        `${componentName} must be inside ${providerName} with a value`,
+      );
+    return c;
+  }
+  return [useCtx, ctx.Provider] as const; // 'as const' makes TypeScript infer a tuple
+}
+
+export { customCreateContext as createContext };
