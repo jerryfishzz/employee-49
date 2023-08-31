5d9b07978819bfcd52f8d4091283f6cdeeb4159d
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-08-31 23:16:28 +1200

    Create custom theme hook
    feat: Create custom theme hook

---
+++ src/hooks/useAppTheme.tsx
@@ -0,0 +1,19 @@
+import { MD3LightTheme, useTheme } from 'react-native-paper';
+
+import { paySauceThemeLight } from 'src/constants/Colors';
+
+const { colors } = paySauceThemeLight;
+
+const theme = {
+  ...MD3LightTheme,
+  colors,
+};
+
+type AppTheme = typeof theme;
+
+// Typed custom theme hook
+function useAppTheme() {
+  return useTheme<AppTheme>();
+}
+
+export { useAppTheme };
