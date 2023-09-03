a0f35325a068469949d2f312c4190189284faa22
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-03 15:11:54 +1200

    Add Dot icon
    feat: add Dot icon component
    
    src/components/ui/icons/Dot.tsx
    
    - Add Dot
    
    src/components/ui/icons/index.ts
    
    - Create index to export
    
    src/components/ui/index.ts
    
    - Add export from icons
    
    src/components/ui/icons/Icon.tsx
    
    - Make style optional

---
+++ src/components/ui/icons/Dot.tsx
@@ -0,0 +1,11 @@
+import { Octicons } from '@expo/vector-icons';
+
+import { Icon } from './Icon';
+
+export function Dot() {
+  return (
+    <Icon>
+      <Octicons name="dot-fill" size={24} color="black" />
+    </Icon>
+  );
+}
--- src/components/ui/icons/Icon.tsx
+++ src/components/ui/icons/Icon.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 import { View } from '../../Themed';
 
 interface IconProps {
-  style: StyleProp<ViewStyle>;
+  style?: StyleProp<ViewStyle>;
   children: ReactNode;
 }
 
