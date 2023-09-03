2620bcb141747a7f8b57d3a98e213c8cf2193a41
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-03 14:17:26 +1200

    Add Icon ui component
    feat: Add Icon ui component

---
+++ src/components/ui/Icon.tsx
@@ -0,0 +1,20 @@
+import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
+import { ReactNode } from 'react';
+
+import { View } from '../Themed';
+
+interface IconProps {
+  style: StyleProp<ViewStyle>;
+  children: ReactNode;
+}
+
+export function Icon({ style, children }: IconProps) {
+  return <View style={[styles.container, style]}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
