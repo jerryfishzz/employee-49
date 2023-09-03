0e3da935196eb59b85bf80df6ff5d4a150f8718f
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-03 16:37:22 +1200

    Add RowDotAndroid
    feat: add RowDotAndroid for the dot in row on Android devices

--- src/components/ui/icons/Dot.tsx
+++ src/components/ui/icons/Dot.tsx
@@ -1,11 +1,26 @@
 import { Octicons } from '@expo/vector-icons';
+import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
 
 import { Icon } from './Icon';
 
-export function Dot() {
+interface DotProps {
+  style?: StyleProp<ViewStyle>;
+}
+
+export function Dot({ style }: DotProps) {
   return (
-    <Icon>
+    <Icon style={style}>
       <Octicons name="dot-fill" size={24} color="yellow" />
     </Icon>
   );
 }
+
+export function RowDotAndroid() {
+  return <Dot style={styles.rowDotAndroid} />;
+}
+
+const styles = StyleSheet.create({
+  rowDotAndroid: {
+    marginLeft: 16,
+  },
+});
