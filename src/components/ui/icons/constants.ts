4466feb829c0b8741fa222a816190da2858194e4
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-03 22:50:33 +1200

    Modify DEFAULT_DOT_COLOR value
    refactor: Modify DEFAULT_DOT_COLOR value

--- src/components/ui/icons/constants.ts
+++ src/components/ui/icons/constants.ts
@@ -1,2 +1,4 @@
+import { paySauceColor } from 'src/constants/Colors';
+
 export const DEFAULT_SIZE = 24;
-export const DEFAULT_DOT_COLOR = 'yellow';
+export const DEFAULT_DOT_COLOR = paySauceColor.hotChilli;
