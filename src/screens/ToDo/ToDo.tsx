14da1433965ce57b86a28e819a54879ce323032d
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-04 19:12:44 +1200

    Remove useless elements
    refactor: Remove useless elements on ToDo screen

--- src/screens/ToDo/ToDo.tsx
+++ src/screens/ToDo/ToDo.tsx
@@ -1,30 +1,18 @@
 import { StyleSheet } from 'react-native';
-import { Text as PaperText } from 'react-native-paper';
 
 import { TaskList } from 'src/components/TaskList';
-import { Text, View } from 'src/components/Themed';
+import { View } from 'src/components/Themed';
 import { taskMap } from 'src/data/task';
 
 export function ToDo() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>TO DO</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <View style={styles.getStartedContainer}>
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)"
-        >
-          Open up the code for this screen:
-        </Text>
-        <PaperText variant="displayLarge">This is paper text</PaperText>
-        <TaskList data={[...taskMap.values()]} />
-      </View>
+      <TaskList data={[...taskMap.values()]} />
     </View>
   );
 }
@@ -32,25 +20,10 @@ export function ToDo() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    alignItems: 'stretch',
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
-  },
-  getStartedContainer: {
-    // alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  getStartedText: {
-    fontSize: 17,
-    lineHeight: 24,
-    textAlign: 'center',
   },
 });
