30d97466c8666306178da75dc0ca1a87baaa6627
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-05 11:20:20 +1200

    Use TaskList on Done screen
    feat: Use TaskList on Done screen

--- src/screens/Done/Done.tsx
+++ src/screens/Done/Done.tsx
@@ -1,18 +1,13 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from 'src/components/EditScreenInfo';
-import { Text, View } from 'src/components/Themed';
+import { TaskList } from 'src/components/TaskList';
+import { View } from 'src/components/Themed';
+import { taskMap } from 'src/data/task';
 
 export function Done() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>DONE</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/done.tsx" />
+      <TaskList data={[...taskMap.values()]} />
     </View>
   );
 }
@@ -20,16 +15,6 @@ export function Done() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    alignItems: 'stretch',
   },
 });
