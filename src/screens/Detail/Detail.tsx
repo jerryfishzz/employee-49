8b02730dc744ef2c19fe2f379662e3af7f8548f3
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-07 19:08:19 +1200

    Add status row
    feat: add status row in Detail screen

--- src/screens/Detail/Detail.tsx
+++ src/screens/Detail/Detail.tsx
@@ -1,12 +1,55 @@
+import { Platform, ScrollView, StyleSheet } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
+import { List } from 'react-native-paper';
+
 import { Text, View } from 'src/components/Themed';
+import {
+  Block,
+  ContentRow,
+  ContentRowAndroid,
+} from 'src/components/ContentRow';
+import { taskMap } from 'src/data/task';
 
 export function Detail() {
   const { id } = useLocalSearchParams();
+  const task = taskMap.get(id as string);
+
+  const statusRow: Block[] = [
+    {
+      type: 'text',
+      content: 'Status',
+      variant: 'titleLarge',
+      viewStyle: {
+        flex: 1,
+      },
+    },
+    {
+      type: 'text',
+      variant: 'titleMedium',
+      content: task?.status,
+    },
+  ];
+
   return (
-    <View>
-      <Text>Detail Screen</Text>
-      <Text>ID: {id}</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text>Detail Screen</Text>
+        <Text>ID: {id}</Text>
+        {Platform.OS === 'android' ? (
+          <>
+            <ContentRowAndroid blocks={statusRow} />
+          </>
+        ) : (
+          <List.Item title={<ContentRow blocks={statusRow} />} />
+        )}
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+  },
+});
