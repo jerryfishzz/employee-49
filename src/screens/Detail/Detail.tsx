7c35d2c2265a3d2fb0ce2461123032276e430954
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-08 00:35:39 +1200

    Refactor scroll view list
    refactor: Refactor scroll view list

--- src/screens/Detail/Detail.tsx
+++ src/screens/Detail/Detail.tsx
@@ -3,51 +3,53 @@ import { List } from 'react-native-paper';
 import date from 'date-and-time';
 
 import { View } from 'src/components/Themed';
-import {
-  Block,
-  ContentRow,
-  ContentRowAndroid,
-} from 'src/components/ContentRow';
+import { ContentRow, ContentRowAndroid } from 'src/components/ContentRow';
 import { Dot, RowDotAndroid } from 'src/components/ui';
 import { PRIORITY } from 'src/data/Priority';
-import { DetailProps } from './types';
+import { DetailProps, DetailRowData } from './types';
 import { createContentBlock, createTitleBlock } from './helpers';
 import { STATUS } from 'src/data/Status';
 
 export function Detail({
   task: { status, description, due, priority },
 }: DetailProps) {
-  const statusBlocks: Block[] = [
-    createTitleBlock('Status'),
-    createContentBlock(STATUS[status]),
-  ];
-
-  const dueBlocks: Block[] = [
-    createTitleBlock('Due'),
-    createContentBlock(date.format(new Date(due), 'D MMMM, YYYY')),
-  ];
-
-  const priorityBlocks: Block[] = [
-    createTitleBlock('Priority'),
-    createContentBlock(
-      `${priority.charAt(0).toUpperCase()}${priority.slice(1)}`,
-    ),
-  ];
-
-  const priorityRightIcon: Block = {
-    type: 'icon',
-    content: <RowDotAndroid iconColor={PRIORITY[priority].color} />,
-  };
-
-  const descriptionBlocks: Block[] = [
+  const detailRowData: DetailRowData[] = [
     {
-      type: 'text',
-      content: description,
-      variant: 'titleMedium',
-      viewStyle: {
-        flex: 1,
-        paddingLeft: Platform.OS === 'android' ? 16 : 0,
+      blocks: [createTitleBlock('Status'), createContentBlock(STATUS[status])],
+    },
+    {
+      blocks: [
+        createTitleBlock('Due'),
+        createContentBlock(date.format(new Date(due), 'D MMMM, YYYY')),
+      ],
+    },
+    {
+      blocks: [
+        createTitleBlock('Priority'),
+        createContentBlock(
+          `${priority.charAt(0).toUpperCase()}${priority.slice(1)}`,
+        ),
+      ],
+      right: {
+        android: {
+          type: 'icon',
+          content: <RowDotAndroid iconColor={PRIORITY[priority].color} />,
+        },
+        others: <Dot iconColor={PRIORITY[priority].color} />,
       },
+    },
+    {
+      blocks: [
+        {
+          type: 'text',
+          content: description,
+          variant: 'titleMedium',
+          viewStyle: {
+            flex: 1,
+            paddingLeft: Platform.OS === 'android' ? 16 : 0,
+          },
+        },
+      ],
     },
   ];
 
@@ -56,27 +58,28 @@ export function Detail({
       <ScrollView>
         {Platform.OS === 'android' ? (
           <>
-            <ContentRowAndroid blocks={statusBlocks} />
-            <ContentRowAndroid blocks={dueBlocks} />
-            <ContentRowAndroid
-              blocks={[...priorityBlocks, priorityRightIcon]}
-            />
-            <ContentRowAndroid blocks={descriptionBlocks} />
+            {detailRowData.map(({ blocks, right }, index) => (
+              <ContentRowAndroid
+                key={index}
+                blocks={right ? [...blocks, right.android] : blocks}
+              />
+            ))}
           </>
         ) : (
           <>
-            <List.Item title={<ContentRow blocks={statusBlocks} />} />
-            <List.Item title={<ContentRow blocks={dueBlocks} />} />
-            <List.Item
-              title={<ContentRow blocks={priorityBlocks} />}
-              right={(props) => (
-                <List.Icon
-                  {...props}
-                  icon={() => <Dot iconColor={PRIORITY[priority].color} />}
-                />
-              )}
-            />
-            <List.Item title={<ContentRow blocks={descriptionBlocks} />} />
+            {detailRowData.map(({ blocks, right }, index) => (
+              <List.Item
+                key={index}
+                title={<ContentRow blocks={blocks} />}
+                right={
+                  right
+                    ? (props) => (
+                        <List.Icon {...props} icon={() => right.others} />
+                      )
+                    : undefined
+                }
+              />
+            ))}
           </>
         )}
       </ScrollView>
--- src/screens/Detail/types.ts
+++ src/screens/Detail/types.ts
@@ -1,5 +1,15 @@
+import { ReactNode } from 'react';
+import { Block } from 'src/components/ContentRow';
 import { Task } from 'src/data/task.schema';
 
 export interface DetailProps {
   task: Task;
+}
+
+export interface DetailRowData {
+  blocks: Block[];
+  right?: {
+    android: Block;
+    others: ReactNode;
+  };
 }
