ab567b0be95a60a164a12e9e1b88eca36bf4eab7
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-07 20:35:17 +1200

    Add due row
    feat: add due row

--- src/screens/Detail/Detail.tsx
+++ src/screens/Detail/Detail.tsx
@@ -1,6 +1,7 @@
 import { Platform, ScrollView, StyleSheet } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import { List } from 'react-native-paper';
+import date from 'date-and-time';
 
 import { Text, View } from 'src/components/Themed';
 import {
@@ -30,6 +31,22 @@ export function Detail() {
     },
   ];
 
+  const dueRow: Block[] = [
+    {
+      type: 'text',
+      content: 'Due',
+      variant: 'titleLarge',
+      viewStyle: {
+        flex: 1,
+      },
+    },
+    {
+      type: 'text',
+      variant: 'titleMedium',
+      content: task && date.format(new Date(task.due), 'D MMMM, YYYY'),
+    },
+  ];
+
   return (
     <View style={styles.container}>
       <ScrollView>
@@ -38,9 +55,13 @@ export function Detail() {
         {Platform.OS === 'android' ? (
           <>
             <ContentRowAndroid blocks={statusRow} />
+            <ContentRowAndroid blocks={dueRow} />
           </>
         ) : (
-          <List.Item title={<ContentRow blocks={statusRow} />} />
+          <>
+            <List.Item title={<ContentRow blocks={statusRow} />} />
+            <List.Item title={<ContentRow blocks={dueRow} />} />
+          </>
         )}
       </ScrollView>
     </View>
