99915802aec71ff715b4d22fc7c7f0405dae3746
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-08 13:08:25 +1200

    Migrate TconBasesProps
    chore: Migrate TconBasesProps to types file

--- src/components/ui/icons/types.ts
+++ src/components/ui/icons/types.ts
@@ -1,7 +1,13 @@
+import { ReactNode } from 'react';
 import { OpaqueColorValue, StyleProp, ViewStyle } from 'react-native';
 
 export interface IconProps {
   style?: StyleProp<ViewStyle>;
   size?: number;
   iconColor?: string | OpaqueColorValue;
+}
+
+export interface IconBaseProps {
+  style?: StyleProp<ViewStyle>;
+  children: ReactNode;
 }
