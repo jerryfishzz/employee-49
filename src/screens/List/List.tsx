68a1ea2ac92e16e315f6757131136fcfd99bb9b6
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-14 12:16:21 +1200

    Create useRefreshOnFocus
    feat: create useRefreshOnFocus hook for List re-fetching
    
    src/screens/List/useRefreshOnFocus.ts
    
    - Create the hook for re-fetching when screen are back to focus
    
    src/screens/List/List.tsx
    
    - Use the re-fetching hook in List
    
    - Remove unused code
    
    src/utils/api.ts
    
    - Add output for tests

--- src/screens/List/List.tsx
+++ src/screens/List/List.tsx
@@ -1,5 +1,4 @@
 import { useQuery } from '@tanstack/react-query';
-import { usePathname } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
 import { TaskList } from 'src/components/TaskList';
@@ -7,16 +6,18 @@ import { View } from 'src/components/Themed';
 import { taskMap } from 'src/data/task';
 import { RootTabParamList, RootTabScreenProps } from 'src/navigation/types';
 import { getTasks } from 'src/utils/api';
+import { useRefreshOnFocus } from './useRefreshOnFocus';
 
 export function List({ route }: RootTabScreenProps<keyof RootTabParamList>) {
-  // const path = usePathname();
-  // console.log(`path: ${path}`);
-  // console.log(`route: ${route.name}`);
-
   useQuery({
     queryKey: ['tasks'],
     queryFn: getTasks,
   });
+
+  // Re-fetch on screen focus
+  // since this screen won't get re-rendered on mobile when switching back.
+  // That causes react query cannot be triggered.
+  useRefreshOnFocus(getTasks);
 
   return (
     <View style={styles.container}>
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,6 +5,7 @@ import { HOST_URL } from 'src/data/host';
 export async function getTasks(): Promise<any> {
   try {
     const response = await axios(`${HOST_URL}`);
+    console.log('getTasks');
     return response.data;
   } catch (e) {
     console.error(e);
@@ -15,6 +16,7 @@ export async function getTasks(): Promise<any> {
 export async function getDetail(id: string): Promise<any> {
   try {
     const response = await axios(`${HOST_URL}/detail/${id}`);
+    console.log('getDetail');
     return response.data;
   } catch (e) {
     console.error(e);
