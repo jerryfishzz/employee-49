0d914ca72d38a483a52cd08ab93c617e1d59b800
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-17 13:27:14 +1200

    Add context for taskMap
    feat: add context and reducer for taskMap

--- src/context/taskMap.ts
+++ src/context/taskMap.ts
@@ -1,4 +1,7 @@
+import { Dispatch } from 'react';
 import { z } from 'zod';
+
+import { createContext } from 'src/utils/context-utils';
 
 export const taskSchema = z.object({
   id: z.string({
@@ -21,3 +24,38 @@ const taskMapSchema = z.map(z.string(), taskSchema);
 
 export type Task = z.infer<typeof taskSchema>;
 export type TaskMap = z.infer<typeof taskMapSchema>;
+
+type Action = {
+  type: 'Complete task' | 'Set to to-do';
+  id: Task['id'];
+};
+
+const [useTaskMap, taskMapContext] = createContext<[TaskMap, Dispatch<Action>]>(
+  '<TaskMap />',
+  'TaskMap',
+);
+
+export function taskMapReducer(state: TaskMap, action: Action) {
+  switch (action.type) {
+    case 'Complete task':
+    case 'Set to to-do': {
+      const currentTask = state.has(action.id) ? state.get(action.id) : null;
+
+      if (!currentTask) return state;
+
+      const newState = new Map(state);
+      newState.set(action.id, {
+        ...currentTask,
+        completed:
+          action.type === 'Complete task' ? new Date().toISOString() : null,
+        status: action.type === 'Complete task' ? 'done' : 'toDo',
+      });
+
+      return newState;
+    }
+    default:
+      throw new Error('Unknown action type');
+  }
+}
+
+export { useTaskMap, taskMapContext };
