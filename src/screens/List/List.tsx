d3d42573a9e4cddcc8b28db709fabc47f7917dbb
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-21 09:46:37 +1200

    Remove unused code
    chore: Remove unused code

--- src/screens/List/List.tsx
+++ src/screens/List/List.tsx
@@ -1,11 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
 import { StyleSheet } from 'react-native';
 
 import { TaskList } from 'src/components/TaskList';
 import { View } from 'src/components/Themed';
 import { RootTabParamList, RootTabScreenProps } from 'src/navigation/types';
-import { getTasks } from 'src/utils/api';
-import { useRefreshOnFocus } from './useRefreshOnFocus';
 import { Task } from 'src/context/taskMap';
 
 type ListProps = {
@@ -13,16 +10,6 @@ type ListProps = {
 } & RootTabScreenProps<keyof RootTabParamList>;
 
 export function List({ data, route }: ListProps) {
-  // useQuery({
-  //   queryKey: ['tasks'],
-  //   queryFn: getTasks,
-  // });
-
-  // Re-fetch on screen focus
-  // since this screen won't get re-rendered on mobile when switching back.
-  // That causes react query cannot be triggered.
-  // useRefreshOnFocus(getTasks);
-
   return (
     <View style={styles.container}>
       <TaskList data={data} routeName={route.name} />
