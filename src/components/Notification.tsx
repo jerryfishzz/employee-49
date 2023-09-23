69dae7e06ba4acb750dfcfef5352d679e3ca6efe
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-23 13:35:23 +1200

    Add a wrapper for notification
    feat: add a wrapper for notification so that absolute position can take effects on iOS

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,24 +1,36 @@
 import { useState } from 'react';
 import { Banner } from 'react-native-paper';
 
+import { View } from './Themed';
+
 export function Notification() {
   const [visible, setVisible] = useState(true);
 
   return (
-    <Banner
-      visible={visible}
-      actions={[
-        {
-          label: 'Fix it',
-          onPress: () => setVisible(false),
-        },
-        {
-          label: 'Learn more',
-          onPress: () => setVisible(false),
-        },
-      ]}
+    <View
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        zIndex: 1,
+      }}
     >
-      There was a problem processing a transaction on your credit card.
-    </Banner>
+      <Banner
+        visible={visible}
+        actions={[
+          {
+            label: 'Fix it',
+            onPress: () => setVisible(false),
+          },
+          {
+            label: 'Learn more',
+            onPress: () => setVisible(false),
+          },
+        ]}
+      >
+        There was a problem processing a transaction on your credit card.
+      </Banner>
+    </View>
   );
 }
