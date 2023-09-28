35a545cff59ddf6fb730b6df91d8682f02779d89
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-09-28 19:48:14 +1300

    Add notification on detail screens
    feat: Add notification on detail screens

--- app/detail/[id].tsx
+++ app/detail/[id].tsx
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import { Redirect, Stack } from 'expo-router';
+import { useEffect } from 'react';
 
+import { hideNotice, showNotice, useEmployee } from 'src/context/employee';
 import { useTaskLocalSearchParams } from 'src/hooks/useTaskLocalSearchParams';
 import { Detail } from 'src/screens/Detail';
 import { Loading } from 'src/screens/Loading';
@@ -9,13 +11,45 @@ import { getDetail } from 'src/utils/api';
 export default function Route() {
   const { id } = useTaskLocalSearchParams();
 
-  const { isLoading, data: task } = useQuery({
+  const {
+    isLoading,
+    isFetching,
+    data: task,
+    isError,
+    error,
+  } = useQuery({
     queryKey: ['detail', id],
     queryFn: () => getDetail(id),
   });
 
-  if (!isLoading && task === undefined) {
-    return <Redirect href="/404" />;
+  const [
+    {
+      notification: { visible },
+    },
+    dispatch,
+  ] = useEmployee();
+
+  // Show notice only when task already exists
+  useEffect(() => {
+    !isLoading &&
+      !isFetching &&
+      isError &&
+      task &&
+      showNotice(dispatch, (error as Error).message);
+  }, [dispatch, error, isError, isFetching, isLoading, task]);
+
+  // Hide previous notice if no errors from query
+  useEffect(() => {
+    !isLoading && !isFetching && !isError && visible && hideNotice(dispatch);
+  }, [dispatch, isError, isFetching, isLoading, visible]);
+
+  // Show notice and redirect
+  if (!isLoading && !isFetching) {
+    if (isError && !task) {
+      showNotice(dispatch, (error as Error).message);
+
+      return <Redirect href="/404" />;
+    }
   }
 
   return (
