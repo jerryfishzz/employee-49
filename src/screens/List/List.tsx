6da74bff764984103c697d8ace2e7ea764515f2b
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2023-10-02 22:21:26 +1300

    Migrate TaskList into List
    refactor: Migrate TaskList into List

--- src/screens/List/List.tsx
+++ src/screens/List/List.tsx
@@ -1,9 +1,34 @@
-import { StyleSheet } from 'react-native';
+import {
+  ColorValue,
+  FlatList,
+  Platform,
+  StyleSheet,
+  ViewStyle,
+} from 'react-native';
+import date from 'date-and-time';
+import { List as PaperList } from 'react-native-paper';
+import { Link } from 'expo-router';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-import { TaskList } from 'src/components/TaskList';
 import { View } from 'src/components/Themed';
+import { paySauceColor } from 'src/data/Colors';
+import { PRIORITY } from 'src/data/Priority';
 import { RootTabParamList, RootTabScreenProps } from 'src/navigation/types';
 import { Task } from 'src/utils/schema';
+import {
+  Dot,
+  Forward,
+  RowDotAndroid,
+  RowForwardAndroid,
+} from 'src/components/ui';
+import { useAppTheme } from 'src/hooks/useAppTheme';
+import {
+  Block,
+  ContentRow,
+  ContentRowAndroid,
+} from 'src/components/ContentRow';
+
+const { hotChilli, midGrey, mint } = paySauceColor;
 
 type ListProps = {
   data: Task[];
@@ -11,20 +36,135 @@ type ListProps = {
 } & RootTabScreenProps<keyof RootTabParamList>;
 
 export function List({ data, isPressDisabled, route }: ListProps) {
+  const {
+    colors: { borderBottom },
+  } = useAppTheme();
+
   return (
     <View style={styles.container}>
-      <TaskList
-        isPressDisabled={isPressDisabled}
+      <FlatList
         data={data}
-        routeName={route.name}
+        renderItem={({ item: { id, title, due, priority }, index }) => {
+          const priorityColor = PRIORITY[priority].color;
+          const dueColor =
+            route.name === 'index'
+              ? isOverdue(due)
+                ? hotChilli
+                : midGrey
+              : mint;
+
+          const titleBlock: Block = {
+            type: 'text',
+            content: title,
+            variant: 'titleLarge',
+            viewStyle: {
+              flex: 1,
+              paddingLeft: Platform.OS === 'android' ? 16 : 0,
+            },
+            textStyle: {
+              fontSize: 20,
+            },
+          };
+
+          const dueBlock: Block = {
+            type: 'text',
+            content: date.format(new Date(due), 'D MMM'),
+            variant: 'titleMedium',
+            textStyle: {
+              color: dueColor,
+            },
+          };
+
+          const dotBlockAndroid: Block = {
+            type: 'icon',
+            content: <RowDotAndroid iconColor={priorityColor} />,
+          };
+
+          const forwardBlockAndroid: Block = {
+            type: 'icon',
+            content: <RowForwardAndroid style={styles.forward} />,
+          };
+
+          const borderBottomStyle = getBorderBottomStyle(
+            borderBottom,
+            index,
+            data.length,
+          );
+
+          if (Platform.OS === 'android')
+            return (
+              <Link href={`/detail/${id}`} asChild>
+                <TouchableOpacity disabled={isPressDisabled}>
+                  <ContentRowAndroid
+                    testID={id}
+                    blocks={[
+                      dotBlockAndroid,
+                      titleBlock,
+                      dueBlock,
+                      forwardBlockAndroid,
+                    ]}
+                    style={borderBottomStyle}
+                  />
+                </TouchableOpacity>
+              </Link>
+            );
+
+          return (
+            <Link href={`/detail/${id}`} asChild>
+              <TouchableOpacity disabled={isPressDisabled}>
+                <PaperList.Item
+                  title={
+                    <ContentRow testID={id} blocks={[titleBlock, dueBlock]} />
+                  }
+                  left={(props) => (
+                    <PaperList.Icon
+                      {...props}
+                      icon={() => <Dot iconColor={priorityColor} />}
+                    />
+                  )}
+                  right={({ style, ...props }) => (
+                    <PaperList.Icon
+                      style={[style, styles.forward]}
+                      {...props}
+                      icon={() => <Forward />}
+                    />
+                  )}
+                  style={[borderBottomStyle, styles.listPadding]}
+                />
+              </TouchableOpacity>
+            </Link>
+          );
+        }}
+        keyExtractor={(item) => item.id}
       />
     </View>
   );
+}
+
+function isOverdue(due: Task['due']) {
+  return new Date(due) < new Date() ? true : false;
+}
+
+function getBorderBottomStyle(
+  color: ColorValue,
+  index: number,
+  dataLength: number,
+): ViewStyle {
+  return {
+    borderBottomColor: color,
+    borderBottomWidth: index === dataLength - 1 ? 0 : 2,
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'stretch',
+  },
+  listPadding: {
+    paddingVertical: 12,
+  },
+  forward: {
+    marginLeft: 4,
   },
 });
